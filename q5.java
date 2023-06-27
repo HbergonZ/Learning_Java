20f2ec773b2b80cb3097db5a2cf53141ab620d17
Hbergon <67556869+HbergonZ@users.noreply.github.com>
2023-06-27 11:46:35 -0400

    Update q5.java

--- q5.java
+++ q5.java
@@ -1,5 +1,5 @@
 package prova_ifro;
-
+/***Elabore um programa que leia os nome de seis alunos e também suas quatro notas bimestrais. Ao final, deve apresentar o nome de cada aluno classificado, bem como sua médias e a média geral dos seis alunos.***/
 import java.util.Scanner;
 
 public class q5 {
@@ -14,8 +14,7 @@ public class q5 {
 		String[] alunos = new String[qtd];
 		double[] medias = new double[qtd];
 		double[] notas = new double[4];
-		double mediaAprovados = 0;
-		int alunosAprovados =0;
+		double somaMedia = 0;
 		
 		for(int i=0;i<qtd;i++) {
 			System.out.printf("Informe o nome da(o) %d° aluna(o):%n",i+1);
@@ -28,17 +27,17 @@ public class q5 {
 			}
 			sc.nextLine();
 			medias[i] = soma/4;
+			somaMedia += medias[i];
 		}
 		
-		System.out.println("-----AUNOS APROVADOS-----");
+		System.out.println("---------AUNOS APROVADOS---------");
 		for(int i=0;i<qtd;i++) {
 			if(medias[i]>=7) {
 				System.out.printf("Aluna(o): %s - Média: %.1f%n",alunos[i],medias[i]);
-				mediaAprovados += medias[i];
-				alunosAprovados++;
 			}
 		}
-		System.out.printf("Média dos alunos aprovados: %.1f%n",mediaAprovados/alunosAprovados);
+		System.out.println("---------------------------------");
+		System.out.printf("Média geral dos alunos: %.1f%n",somaMedia/qtd);
 		
 		sc.close();
 
